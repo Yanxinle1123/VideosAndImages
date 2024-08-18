1e616d4e88042975cbeeb66958712c18b975b4d0
yanxinle <1020121123@qq.com>
2024-08-18 15:12:05 +0800

    更新 -完善功能

--- videos_to_images/videos_to_images.py
+++ videos_to_images/videos_to_images.py
@@ -1,3 +1,5 @@
+import asyncio
+import concurrent.futures
 import time
 
 import cv2
@@ -13,18 +15,18 @@ def is_camera(video_source):
 
 
 # 保存图片
-def save_image(image, addr, name, suffix='.png'):
-    address = addr + str(name) + suffix
-    cv2.imwrite(address, image)
 
 
 class VideosToImages:
-    def __init__(self, video_source, save_path, suffix='.png'):
+    def __init__(self, video_source, save_path, suffix='.png', frame_interval_ms=1000, save_name='number'):
         self._video_source = video_source
         self._save_path = save_path
         self._suffix = suffix
+        self._frame_interval_ms = frame_interval_ms
+        self._save_name = save_name
+        asyncio.run(self.convert())
 
-    def convert(self, frame_interval_ms=1000):
+    async def convert(self):
         video_capture = cv2.VideoCapture(self._video_source)
         is_cam = is_camera(self._video_source)
         if is_cam:
@@ -34,28 +36,48 @@ class VideosToImages:
         fps = int(video_capture.get(cv2.CAP_PROP_FPS))
 
         # 计算每隔 frame_interval_ms 毫秒应该跳过的帧数
-        skip_frames = fps * frame_interval_ms // 1000
+        skip_frames = fps * self._frame_interval_ms // 1000
 
         # 截图
         i = 0
         j = 1
-        while video_capture.isOpened():
-            success, frame = video_capture.read()
-            if not success:
-                break
+        tasks = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            while video_capture.isOpened():
+                success, frame = video_capture.read()
+                if not success:
+                    break
 
-            if i % skip_frames == 0:
-                save_image(frame, self._save_path, j, self._suffix)
-                j += 1
-            i += 1
+                if i % skip_frames == 0:
+                    loop = asyncio.get_running_loop()
+                    task = loop.run_in_executor(executor, self._save_image, frame, self._save_path, j, self._suffix)
+                    tasks.append(task)
+                    j += 1
+                i += 1
+
+            # 等待所有任务完成
+            await asyncio.gather(*tasks)
 
         video_capture.release()
         cv2.destroyAllWindows()
 
+    def _save_image(self, image, addr, name, suffix='.png'):
+        save_name = name
+        if self._save_name == 'time':
+            save_name = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+        address = addr + str(save_name) + suffix
+        cv2.imwrite(address, image)
+
 
 if __name__ == '__main__':
+    start_time = time.time()
+
     video_path = 'https://img.tukuppt.com/video_show/2475824/00/01/84/5b4b1d6d2b582.mp4'
     save_path = 'save_images/fallen_leaves2_images/'
 
-    vti = VideosToImages(video_source=video_path, save_path=save_path, suffix='.png')
-    vti.convert(frame_interval_ms=1000)
+    VideosToImages(video_source=video_path, save_path=save_path, suffix='.png', frame_interval_ms=1000,
+                   save_name='time')
+
+    end_time = time.time()
+
+    print('运行时长:', end_time - start_time)
